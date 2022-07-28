18e9c6487e4ba63932a9d2c253943b7893419905
Andre Hioki <ahdreki@gmail.com>
2022-07-27 21:11:35 -0300

    .

---
+++ exercicios/exercicio_3.py
@@ -0,0 +1,7 @@
+# Exercício 3
+# Crie um programa que leia dois número se mostra a soma entre eles.
+
+n1 = int(input("Digite o primeiro número: "))
+n2 = int(input("Digite o segundo número: "))
+s = n1 + n2
+print("A soma entre {} e {} é igual a {}".format(n1, n2, s))
