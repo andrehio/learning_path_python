2890c4931a08b1b9c5f4171a42638db6c343e83a
Andre Hioki <ahdreki@gmail.com>
2022-07-28 21:39:51 -0300

    .

--- exercicios/exercicio_5.py
+++ exercicios/exercicio_5.py
@@ -1,4 +1,11 @@
 # Exercício 5
-# Crie um programa que escreva "Olá Mundo" na tela.
+# Faça um programa que leia um número inteiro e mostre na tela o seu
+# sucessor e o seu antecessor.
 
-print("Olá Mundo")
+entrada = input("Digite um número: ")
+if entrada.isnumeric():
+    num = int(entrada)
+    print("Analisando o valor {}, seu antecessor é {} e o sucessor é {"
+          "}".format(num, num - 1, num + 1))
+else:
+    print("Digite um número!")
