09ed5915b250ee2c3e082a67de0e9e1f31307fed
Andre Hioki <ahdreki@gmail.com>
2022-07-30 11:55:36 -0300

    14- conversor de temperaturas

--- exercicios/exercicio_0014.py
+++ exercicios/exercicio_0014.py
@@ -1,8 +1,10 @@
 # Exercício 14
 # Escreva um programa que converta uma temperatura digitada em ºC e converta
-# para ºF.
+# para ºF e K.
 
 c = float(input("Informe a temperatura em ºC: "))
 f = 9 * c / 5 + 32
-print("A temperatura de {:.1f}ºC corresponde a {:.1f}ºF".format(c, f))
-
+k = c + 273.15
+print("A temperatura de {:.1f}ºC corresponde a {:.1f}ºF e {:.1f}K".format(c,
+                                                                          f,
+                                                                          k))
