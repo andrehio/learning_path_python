8fb96d6263caf4ccb35de3fc09264e83ebd628e0
Andre Hioki <ahdreki@gmail.com>
2022-07-30 11:49:28 -0300

    .

--- exercicios/exercicio_2.py
+++
@@ -1,6 +0,0 @@
-# Exercício 2
-# Faça um programa que leia o nome de uma pessoa e mostre uma
-# mensagem de boas-vindas.
-
-nome = input("Digite o seu nome: ")
-print("Prazer em te conhecer, {}!".format(nome))
