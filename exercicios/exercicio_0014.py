88b7e81fde6c5d6f502d82c0bdc72e773293daad
Andre Hioki <94112917+andrehio@users.noreply.github.com>
2022-07-30 14:12:12 -0300

    14- converter temperaturas

--- exercicios/exercicio_0014.py
+++ exercicios/exercicio_0014.py
@@ -5,6 +5,5 @@
 c = float(input("Informe a temperatura em ºC: "))
 f = 9 * c / 5 + 32
 k = c + 273.15
-print("A temperatura de {:.1f}ºC corresponde a {:.1f}ºF e {:.1f}K".format(c,
-                                                                          f,
-                                                                          k))
+print("A temperatura de {:.1f}ºC corresponde a {:.1f}ºF e {:.1f}K"
+      .format(c, f, k))
